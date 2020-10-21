f3d9b8c06ebb8e2b9d89b50fc33cbaf87a08d79e
andriidem308 <andriidem308@gmail.com>
2020-10-21 14:20:59 +0300

    "task 3.1_OOP completed"

--- src/topic_3_OOP/task_1.java
+++ src/topic_3_OOP/task_1.java
@@ -8,57 +8,133 @@ package topic_3_OOP;
 import java.util.Scanner;
 
 public class task_1 {
+    public static class Fraction{
+        int m, n;
+
+        public Fraction(int m, int n){
+            if (n == 0)
+                System.exit(1);
+            this.m = m;
+            this.n = n;
+        }
+
+        public double getValue(){
+            return (double) m / n;
+        }
+
+        public void add(Fraction other){
+            if (this.n == other.n){
+                this.m += other.m;
+            }
+            else {
+                int m1 = this.m;
+                int n1 = this.n;
+
+                this.m = m1*other.n + other.m*n1;
+                this.n = n1 * other.n;
+            }
+
+            update();
+        }
+
+        public void subtract(Fraction other){
+            if (this.n == other.n){
+                this.m -= other.m;
+            }
+            else {
+                int m1 = this.m;
+                int n1 = this.n;
+
+                this.m = m1*other.n - other.m*n1;
+                this.n = n1 * other.n;
+            }
+
+            update();
+        }
+
+        public void multiply(Fraction other){
+            this.m *= other.m;
+            this.n *= other.n;
+
+            update();
+        }
+
+        public void divide(Fraction other){
+            this.m *= other.n;
+            this.n *= other.m;
+
+            update();
+        }
+
+        public int gcd(){
+            int a = m, b = n;
+
+            while (a != b) {
+                if (a > b){
+                    a -= b;
+                } else {
+                    b -= a;
+                }
+            }
+
+            return b;
+        }
+
+        public void update(){
+            if (m % n == 0){
+                this.m = m / n;
+                this.n = 1;
+            } else if (gcd() > 1){
+                int var_gcd = gcd();
+                this.m /= var_gcd;
+                this.n /= var_gcd;
+            }
+        }
+
+        @Override
+        public String toString() {
+            return m + "/" + n;
+        }
+    }
+
     public static class Polynomial {
-        private double[] coefficients;
+        public static int max_len;
+        public Fraction[] coefficients;
         private int size;
 
         Polynomial(){}
 
         Polynomial(int size){
-            coefficients = new double[size];
+            this.coefficients = new Fraction[size];
             this.size = size;
-        }
-
-        Polynomial(Double... argsArray) {
-            size = argsArray.length;
-            coefficients = new double[size];
-
-            for (int i = 0; i < size; i++){
-                coefficients[i] = argsArray[size - 1 - i];
-            }
-        }
-
-        public void consoleInput(){
-            Scanner scanner = new Scanner(System.in);
-            String[] strCoeffsArr = scanner.nextLine().split(" ");
-            size = strCoeffsArr.length;
-            coefficients = new double[size];
-
-            for (int i = 0; i < size; i++){
-                coefficients[i] = Double.parseDouble(strCoeffsArr[size-1-i]);
-            }
-        }
-
-        public double calc(double x){
-            double result = 0;
-
+            if (size > max_len) max_len = size;
             for (int i = 0; i < size; i++)
-                result += coefficients[i] * Math.pow(x, i);
-
-            return result;
+                this.coefficients[i] = new Fraction(0, 1);
         }
+
+        Polynomial(Integer... argsArray) {
+            size = argsArray.length / 2;
+
+            if (size > max_len) max_len = size;
+            coefficients = new Fraction[size];
+
+            for (int i = 0; i < argsArray.length; i+=2){
+                int m1, n1;
+                m1 = argsArray[i];
+                n1 = argsArray[i+1];
+
+                coefficients[size - i/2 - 1] = new Fraction(m1, n1);
+            }
+        }
+
 
         @Override
         public String toString() {
             String out = "";
 
             for (int i = size-1; i >= 0; i--) {
-                if (coefficients[i] == 0.0) continue;
-                if (coefficients[i] < 0.0 && !out.isEmpty()){
-                    out = out.substring(0, out.length()-2) + "- ";
-                }
-                if (out.isEmpty()) out += "-";
-                out += Math.abs(coefficients[i]) + "x^" + i + " + ";
+                if (coefficients[i].m == 0.0) continue;
+                out += coefficients[i] + "x^" + i + " + ";
             }
 
             out = out.substring(0, out.length() - 3);
@@ -71,9 +147,10 @@ public class task_1 {
             int max_len = Math.max(len_1, len_2);
 
             Polynomial new_poly = new Polynomial(max_len);
+
             for (int i = 0; i < max_len; i++){
-                if (i < p1.size) new_poly.coefficients[i] += p1.coefficients[i];
-                if (i < p2.size) new_poly.coefficients[i] += p2.coefficients[i];
+                if (i < len_1) new_poly.coefficients[i].add(p1.coefficients[i]);
+                if (i < len_2) new_poly.coefficients[i].add(p2.coefficients[i]);
             }
 
             return new_poly;
@@ -81,14 +158,9 @@ public class task_1 {
     }
 
     public static Polynomial sumPolys(Polynomial[] polynomials){
-        int polysAmount = polynomials.length;
-
-        if (polysAmount < 2) return new Polynomial();
-        Polynomial summaryPoly = Polynomial.add(polynomials[0], polynomials[1]);
-        if (polysAmount == 2) return summaryPoly;
-
-        for (int i = 2; i < polysAmount; i++){
-            summaryPoly = Polynomial.add(summaryPoly, polynomials[i]);
+        Polynomial summaryPoly = new Polynomial(Polynomial.max_len);
+        for (Polynomial polynomial : polynomials) {
+            summaryPoly = Polynomial.add(summaryPoly, polynomial);
         }
 
         return summaryPoly;
@@ -103,78 +175,28 @@ public class task_1 {
     }
 
     public static Polynomial[] autoMode(){
-        Polynomial[] polynomials = new Polynomial[6];
-        polynomials[0] = new Polynomial(3.0, 4.0, 1.0, 2.0, -3.0, 0.0);
-        polynomials[1] = new Polynomial(4.0, 6.0, 5.0, 0.0, -1.0, 1.0, -2.0, 8.0);
-        polynomials[2] = new Polynomial(-2.0, -3.0, 0.0, 0.0, -4.0, 9.0, -3.0);
-        polynomials[3] = new Polynomial(-3.0, 1.0, 1.0);
-        polynomials[4] = new Polynomial(-2.0, 5.0, 0.0, 2.0, 2.0);
-        polynomials[5] = new Polynomial(4.0, 4.0, 5.0, 2.0, 5.0, -4.0, 7.0, 8.0);
+        Polynomial[] polynomials = new Polynomial[8];
+        polynomials[0] = new Polynomial(1, 2, 2, 3, 7, 5);
+        polynomials[1] = new Polynomial(3, 4, 5, 3, 1, 5);
+        polynomials[2] = new Polynomial(2, 4, 2, 3, 8, 5);
+        polynomials[3] = new Polynomial(2, 4, -1, 3, 8, 5);
+        polynomials[4] = new Polynomial(-2, 4, 1, 3, 8, 5);
+        polynomials[5] = new Polynomial(2, 4, 5, 2, 8, 5);
+        polynomials[6] = new Polynomial(2, 4, -1, 3, 8, 5);
+        polynomials[7] = new Polynomial(2, 4, 2, 3, 8, 5);
 
         return polynomials;
-    }
-
-    public static Polynomial[] consoleMode(){
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        Polynomial[] polynomials = new Polynomial[n];
-
-        for (int i = 0; i < n; i++){
-            polynomials[i] = new Polynomial();
-            polynomials[i].consoleInput();
-        }
-
-        return polynomials;
-    }
-
-    public static void testCalculating(){
-        System.out.println("--- --- --- --- --- TEST CALCULATING --- --- --- --- ---");
-
-        Polynomial poly = new Polynomial(1.0, 3.0, -1.0, 11.0);
-        System.out.println(poly);
-        System.out.println("x = 2: " + poly.calc(2.0));
-        System.out.println("x = -4: " + poly.calc(-4.0));
-
-        System.out.println("\n-------------------------------------------------------");
-    }
-
-    public static void testPolysAddition(){
-        System.out.println("--- --- --- --- --- TEST ADDITION --- --- --- --- ---");
-
-        Polynomial poly_1 = new Polynomial(2.0, 7.0, -1.0, -3.0, 0.0, 2.0, -4.0);
-        Polynomial poly_2 = new Polynomial(3.0, 8.0, 0.0, -6.0, 1.0);
-        Polynomial poly_3 = new Polynomial(-5.0, 11.0, 5.0, 5.5, 2.2, 0.0, -6.0, 1.0);
-
-        System.out.println("Polynomial 1: " + poly_1);
-        System.out.println("Polynomial 2: " + poly_2);
-        System.out.println("Polynomial 3: " + poly_3);
-
-        System.out.println("\nP1 + P2: " + Polynomial.add(poly_1, poly_2));
-        System.out.println("P2 + P3: " + Polynomial.add(poly_2, poly_3));
-        System.out.println("P3 + P1: " + Polynomial.add(poly_3, poly_1));
-
-        System.out.println("\n-------------------------------------------------------");
     }
 
     public static void mainTest(){
-        System.out.println("--- --- --- --- --- MAIN TEST --- --- --- --- ---");
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("\n*** ChooseMode ***\n\t0 - Console Mode\n\tOther numbers - Auto Mode\nYour choice: ");
-        int mode = scanner.nextInt();
-
-        Polynomial[] polynomials = (mode == 0) ? consoleMode() : autoMode();
-        printArray(polynomials);
+        Polynomial[] polynomials = autoMode();
 
         Polynomial summaryPolynomial = sumPolys(polynomials);
         System.out.println(summaryPolynomial);
-
-        System.out.println("\n-------------------------------------------------------");
+        System.out.println("-------------------------------------------------------");
     }
 
     public static void main(String[] args) {
-        testCalculating();
-        testPolysAddition();
         mainTest();
     }
 }
